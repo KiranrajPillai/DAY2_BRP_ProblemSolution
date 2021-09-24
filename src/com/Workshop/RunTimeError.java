70695538ccb0ec34aaa0e14e704a19707f99992a
KiranrajPillai <kiranrajpillai1997@gmail.com>
2021-09-24 15:38:31 +0530

    Check Run Time Error.

---
+++ src/com/Workshop/RunTimeError.java
@@ -0,0 +1,11 @@
+package com.Workshop;
+
+public class RunTimeError {
+        public static void main (String[]args){
+            RunTimeError r = new RunTimeError();
+            r.add();
+    }
+//    private void add() {
+//    }
+}
+
